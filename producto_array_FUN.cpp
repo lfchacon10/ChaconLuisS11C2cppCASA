0f4bc4c8ff3a74101c83edf3538a917b6c792dff
lfchacon10 <lf.chacon10@uniandes.edu.co>
2019-04-04 13:50:14 -0500

    solucionado

--- producto_array_FUN.cpp
+++ producto_array_FUN.cpp
@@ -8,7 +8,18 @@ int b[5]={10,20,30,40,50};
 	
 int fun1( int a[], int b[] ) 
 {
-	int respuesta =0;
+	int respuesta[5];
+	for( int i=0; i < tamano; i++)
+	{
+		respuesta[i]= a[i]*b[i];
+		cout<< respuesta[i]<<endl;
+	}		
+}
+
+
+int fun2( int a[], int b[] ) 
+{
+	int respuesta;
 	for( int i=0; i < tamano; i++)
 	{
 		respuesta += a[i]*b[i];
@@ -16,25 +27,11 @@ int fun1( int a[], int b[] )
 	return respuesta;
 }
 
-
-int fun2( int a ) 
-{
-	magA=0;
-	magB=0;
-	for( int i=0; i < tamano; i++)
-	{
-		magA+=pow(a[i],2);
-		magB+=pow(b[i],2);
-	}		
-	return respuesta;
-}
-	return a;
-}
-
 int main(){
 	int a[5]= {1,2,3,4,5};
 	int b[5]={10,20,30,40,50};
-	cout<<"El producto entre a y b :"<<fun1(a,b)<<endl;
+	cout<<"El producto entre a y b es:"<<fun1(a,b)<<endl;
+	cout<<"El producto punto entre a y b es:"<<fun2(a,b)<<endl;
 	return 0;
 
 }
